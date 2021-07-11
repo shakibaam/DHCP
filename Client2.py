5d600a99daf86cde8317791e1be1b82506bee8b0
shakibaam <shakibaamirshahi@yahoo.com>
2021-07-11 21:18:48 +0330

    some othher files added

---
+++ Client2.py
@@ -0,0 +1,275 @@
+import ipaddress
+import socket
+import random
+import socket
+import struct
+import plistlib
+from uuid import getnode as get_mac
+from random import randint
+from time import *
+import threading
+from goto import with_goto
+
+import dhcppython
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('0.0.0.0', 67))
+Mac = ""
+XID = ""
+TIMEOUT = 10
+BACKOFF_CUTOFF = 120
+INITIAL_INTERVAL = 10
+dis_time = 10
+
+serverPort = 67
+clientPort = 68
+
+
+def buildPacket_discovery(mac):
+    mac = str(mac).replace(":", "")
+    mac = bytes.fromhex(mac)
+    global Mac, XID
+    # macb = getMacInBytes()
+    Mac = mac
+    print(Mac)
+    transactionID = b''
+
+    for i in range(4):
+        t = randint(0, 255)
+        transactionID += struct.pack('!B', t)
+    XID = transactionID
+    print(XID)
+
+    packet = b''
+    packet += b'\x01'  # Message type: Boot Request (1)
+    packet += b'\x01'  # Hardware type: Ethernet
+    packet += b'\x06'  # Hardware address length: 6
+    packet += b'\x00'  # Hops: 0
+    packet += transactionID  # Transaction ID
+    packet += b'\x00\x00'  # Seconds elapsed: 0
+    packet += b'\x80\x00'  # Bootp flags:
+    packet += b'\x00\x00\x00\x00'  # Client IP address: 0.0.0.0
+    packet += b'\x00\x00\x00\x00'  # Your (client) IP address: 0.0.0.0
+    packet += b'\x00\x00\x00\x00'  # Next server IP address: 0.0.0.0
+    packet += b'\x00\x00\x00\x00'  # Relay agent IP address: 0.0.0.0
+    packet += mac  # Client MAC address:  "FF:C1:9A:D6:3E:00
+
+    packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    packet += b'\x00' * 67  # Server host name
+    packet += b'\x00' * 125  # Boot file nam
+    packet += b'\x63\x82\x53\x63'  # Magic cookie: DHCP
+    # DHCP IP Address
+    packet += b'\x35\x01\x01'
+
+    return packet
+
+
+def buildPacket_request(serverip, offerip):
+    offerip = bytes(map(int, str(offerip).split('.')))
+    serverip = bytes(map(int, str(serverip).split('.')))
+    global Mac
+
+    packet = b''
+    packet += b'\x01'  # Message type: Boot Request (1)
+    packet += b'\x01'  # Hardware type: Ethernet
+    packet += b'\x06'  # Hardware address length: 6
+    packet += b'\x00'  # Hops: 0
+
+    # print(xid_hex)
+    packet += XID  # Transaction ID
+    packet += b'\x00\x00'  # Seconds elapsed: 0
+    packet += b'\x80\x00'  # Bootp flags: 0x8000 (Broadcast) + reserved flags
+    packet += b'\x00\x00\x00\x00'  # Client IP address: 0.0.0.0
+    packet += offerip  # Your (client) IP address: 0.0.0.0
+    packet += serverip  # Next server IP address: 0.0.0.0
+    packet += b'\x00\x00\x00\x00'  # Relay agent IP address: 0.0.0.0
+    # packet += b'\x00\x26\x9e\x04\x1e\x9b'   #Client MAC address: 00:26:9e:04:1e:9b
+    packet += Mac
+    packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'  # Client hardware address padding: 00000000000000000000
+    packet += b'\x00' * 67  # Server host name not given
+    packet += b'\x00' * 125  # Boot file name not given
+    packet += b'\x63\x82\x53\x63'  # Magic cookie: DHCP
+    # DHCP IP Address
+    packet += b'\x35\x01\x03'  # Option: (t=53,l=1) DHCP Message Type = DHCP Discover
+
+    return packet
+
+
+def pkt_type(packet):
+    if packet[len(packet) - 1] == 2:
+        return "DHCPOFFER"
+    if packet[len(packet) - 1] == 5:
+        return "DHCPACK"
+
+
+def parse_packet_client(pkt):
+    yiaddr_bytes = pkt[16:20]
+    yiaddr_original = ipaddress.IPv4Address(yiaddr_bytes)
+    siaddr_bytes = pkt[20:24]
+    siaddr_original = ipaddress.IPv4Address(siaddr_bytes)
+    mac_byte = pkt[28:34]
+    mac_original = mac_byte.hex(":")
+
+    return yiaddr_original, siaddr_original, mac_original
+
+
+def start_process(mac):
+    global dis_time
+    sock.settimeout(2)
+    sock.sendto(buildPacket_discovery(mac), ('<broadcast>', 68))
+    get_ip = False
+    getAck = False
+    # timer_thread=threading.Thread(target=discovery_timer,args=(ds_time,))
+    # timer_thread.start()
+    # while dis_time>0:
+
+    # offer
+
+    msg, b = sock.recvfrom(1024)
+    try:
+        data = msg.decode()
+        if "reserved" in data:
+            getAck = True
+            get_ip = True
+        print(data)
+    except (UnicodeDecodeError, AttributeError):
+        print(pkt_type(msg))
+        offerip, serverip, mac = parse_packet_client(msg)
+        print("offer {} for {}:".format(offer_ip, mac))
+        print(offerip)
+
+        sock.sendto(buildPacket_request(serverip, offerip), (str(serverip), 68))
+        print("send request")
+        getAck = False
+
+        try:
+            msg, b = sock.recvfrom(1024)
+            if msg:
+                print("Ack {}".format(msg))
+                getAck = True
+        except socket.timeout:
+            print("Time out ...")
+
+        if getAck == False:
+            print("time out!!")
+            # continue
+        else:
+            print("No time out :)")
+            get_ip = True
+
+    return getAck, get_ip
+
+
+def start_process2(mac):
+    global dis_time
+    sock.settimeout(4)
+    sock.sendto(buildPacket_discovery(mac), ('<broadcast>', 68))
+    get_ip = False
+    getAck = False
+    while not getAck:
+        msg, b = sock.recvfrom(1024)
+        try:
+            data = msg.decode()
+            if "reserved" in data:
+                getAck = True
+                get_ip = True
+            print(data)
+        except (UnicodeDecodeError, AttributeError):
+            print(pkt_type(msg))
+            offerip, serverip, mac = parse_packet_client(msg)
+            print("offer {} for {}:".format(offer_ip, mac))
+            print(offerip)
+
+            sock.sendto(buildPacket_request(serverip, offerip), (str(serverip), 68))
+            print("send request")
+            # getAck = False
+
+            try:
+                msg, b = sock.recvfrom(1024)
+
+                if msg:
+                    print("Ack {}".format(msg))
+                    getAck = True
+            except socket.timeout:
+                print("Time out ...")
+                getAck = False
+                continue
+
+            if getAck == False:
+                print("time out!!")
+                # continue
+            else:
+                print("No time out :)")
+                get_ip = True
+
+        return getAck, get_ip
+
+
+def discovery_timer(initial_interval):
+    global dis_time
+    dis_time = initial_interval
+
+    while dis_time:
+        mins, secs = divmod(dis_time, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        # print(timer)
+        sleep(1)
+        dis_time -= 1
+
+
+def time_out():
+    global timeOut
+    timeOut = 3
+
+    while timeOut > 0:
+        mins, secs = divmod(timeOut, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer)
+        sleep(1)
+        timeOut -= 1
+
+
+if __name__ == '__main__':
+
+    def discovery_timer(initial_interval):
+        global dis_time
+        dis_time = initial_interval
+
+        while dis_time:
+            mins, secs = divmod(dis_time, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            # print(timer)
+            sleep(1)
+            dis_time -= 1
+
+
+    mac = input("Enter your mac address")
+
+    offer_ip = ""
+    flag = True
+    getAck = False
+    getIp = False
+
+    timer_thread = threading.Thread(target=discovery_timer, args=(INITIAL_INTERVAL,))
+    timer_thread.start()
+    prv_dis = INITIAL_INTERVAL
+
+
+
+    while dis_time > 0:
+        while not getAck:
+            getAck, getIp = start_process(mac)
+
+    if dis_time <= 0:
+        if getAck == False or getIp == False:
+            dis_time = prv_dis * 2 * random.uniform(0, 1)
+            prv_dis = dis_time
+
+        # if not getIp:
+        #     while ds_time>0:
+        #         print(ds_time)
+        #         pass
+        #
+        #     ds_time = ds_time * 2 * random.uniform(0, 1)
---
+++ Server2.py
@@ -0,0 +1,341 @@
+import ipaddress
+import json
+import struct
+import threading
+import socket
+import logging
+import time
+import random
+from OuiLookup import OuiLookup
+from concurrent.futures import ThreadPoolExecutor  # Python 3.2
+
+
+class Server():
+
+    def __init__(self):
+        print('Initializing DHCP Server')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.bind(('', 68))
+        self.connected_clients_list = dict()
+        # self.clients = []
+        self.OccupyIP = []
+        self.waitIP=[]
+        self.Serviced_ClientsInfo_print = []
+        self.client_ips = dict()
+        self.reserved = dict()
+        self.leaseThreads = dict()
+
+        self.startInterval = 0
+        self.stopInterval = 0
+        self.serverIP = socket.gethostbyname(socket.gethostname())
+        f = open("C:\\Users\\Asus\\PycharmProjects\\CN_P3\\configs.json")
+        data = json.load(f)
+        f.close()
+        self.pool_mode = data["pool_mode"]
+        self.range_from = data["range"]["from"]
+        self.range_to = data["range"]["to"]
+        self.subnet_block = data["subnet"]["ip_block"]
+        self.subnet_mask = data["subnet"]["subnet_mask"]
+        self.lease_time = data["lease_time"]
+        if self.pool_mode == "range":
+            self.startInterval = self.ip2long(self.range_from)
+            self.stopInterval = self.ip2long(self.range_to)
+        elif self.pool_mode == "subnet":
+            self.startInterval = self.ip2long(self.subnet_block)
+            self.stopInterval = self.ip2long(self.subnet_mask)
+        if len(data["reservation_list"]) != 0:
+            for key in data["reservation_list"]:
+                self.reserved[key] = data["reservation_list"][key]
+                self.OccupyIP.append(data["reservation_list"][key])
+
+    def handle_client(self, xid, mac, addrss,server):
+
+        if mac not in self.client_ips:
+            macUpper = str(mac).upper()
+            block = self.block_or_not(macUpper)
+            reserve = self.reserved_or_not(macUpper)
+
+            print(reserve)
+            if block:
+                print("This client is blocked")
+                string = "You are blocked "
+                self.sock.sendto(string.encode(), ('255.255.255.255', 67))
+            if reserve:
+                reserved_ip = self.reserved[macUpper]
+                print("This client is reserved with ip {}".format(reserved_ip))
+                string = "You are reserved with ip {}".format(reserved_ip)
+                self.sock.sendto(string.encode(), ('255.255.255.255', 67))
+                PCName = OuiLookup().query(mac)
+                client_info = [PCName, mac, reserved_ip, "infinity"]
+                self.Serviced_ClientsInfo_print.append(client_info)
+
+            if not block and not reserve:
+                if mac not in self.connected_clients_list:
+                    self.connected_clients_list[mac] = xid
+                # print(self.connected_clients_list)
+                occupy_ip_len = len(self.OccupyIP)
+                all_ip_number = self.stopInterval - self.startInterval + 1
+                if occupy_ip_len == all_ip_number:
+                    string = "sorry all ips are occupied"
+                    self.sock.sendto(string.encode(), ('255.255.255.255', 67))
+                else:
+                    flag = True
+                    offer = 0
+                    offer_ip = ""
+                    while (flag):
+
+                        offer = random.randint(self.startInterval, self.stopInterval)
+                        offer_ip = self.long2ip(offer)
+
+                        if offer_ip in self.OccupyIP and offer_ip in self.waitIP :
+                            continue
+                        else:
+                            print("Server want offer {}".format(offer_ip))
+                            self.waitIP.append(offer_ip)
+
+                            flag = False
+                    print("lets offer to {}".format(mac))
+                    pkt = self.buildPacket_offer(offer_ip, xid, mac)
+                    self.sock.sendto(pkt, ('255.255.255.255', 67))
+                    msg, client = server.recvfrom(1024)
+
+                    print(self.packet_type(msg))
+                    id, chaddrss = self.parse_packet_server(msg)
+                    # yiaddr_bytes = msg[16:20]
+                    # yiaddr_original = ipaddress.IPv4Address(yiaddr_bytes)
+                    # print("client want {}".format(yiaddr_original))
+
+                    pkt = self.buildPacket_Ack(offer_ip, xid, mac)
+                    # start lease time timer
+                    time.sleep(5)
+                    self.sock.sendto(pkt, ('255.255.255.255', 67))
+                    lease_time = self.lease_time
+                    PCName = OuiLookup().query(mac)
+                    client_info = [PCName, mac, offer_ip, lease_time]
+                    self.Serviced_ClientsInfo_print.append(client_info)
+                    index = self.Serviced_ClientsInfo_print.index(client_info)
+                    self.OccupyIP.append(offer_ip)
+                    self.client_ips[mac] = offer_ip
+                    lease_thread = threading.Thread(target=self.lease, args=(mac, offer_ip, xid, index))
+                    self.leaseThreads[mac] = lease_thread
+                    lease_thread.start()
+
+
+
+        else:
+            prev_ip = self.client_ips[mac]
+            prev_thread = self.leaseThreads[mac]
+            print("You are in list yet with {} ,lease time renew".format(prev_ip))
+            string = "You are in list yet with {} ,lease time renew".format(prev_ip)
+            self.sock.sendto(string.encode(), ('255.255.255.255', 67))
+            index = -1
+            prev_thread.join()
+            self.leaseThreads.pop(mac)
+            lease_thread = threading.Thread(target=self.lease, args=(mac, prev_ip, xid, index))
+            self.leaseThreads[mac] = lease_thread
+            lease_thread.start()
+
+    def get_discovery(self):
+        workers = 5
+        items = 15
+        executor = ThreadPoolExecutor(max_workers=workers)
+        executor.submit(self.show_clients())
+        # show_client_thread = threading.Thread(target=self.show_clients())
+        # show_client_thread.start()
+        while True:
+            msg, client = self.sock.recvfrom(1024)
+
+            print('Received data from client {} {}'.format(client, msg))
+            msg_type = self.packet_type(msg)
+            print(msg_type)
+            if "DHCPDISCOVER" in msg_type:
+                client_xid, client_mac = self.parse_packet_server(msg)
+                print("Client xid {}".format(client_xid))
+                server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                server.bind(("127.0.0.1", 68))
+
+
+                executor.submit(self.handle_client,client_xid, client_mac, client,server)
+
+            # t = threading.Thread(target=self.handle_client, args=(client_xid, client_mac, client,))
+            # # self.connected_clients_list[client_xid]=client_mac
+            # t.start()
+
+
+    def packet_type(self, packet):
+        if packet[len(packet) - 1] == 1:
+            return "DHCPDISCOVER"
+        if packet[len(packet) - 1] == 3:
+            return "DHCPREQUEST"
+
+
+    def ip2long(self, ip):
+        packedIP = socket.inet_aton(ip)
+        return struct.unpack("!L", packedIP)[0]
+
+
+    def long2ip(self, data):
+        return socket.inet_ntoa(struct.pack('!L', data))
+
+
+    def isReserved(self, ip):
+        Reserved = False
+        split = str(ip).split(".")
+        if split[len(split) - 1] == "0" or split[len(split) - 1] == "1":
+            Reserved = True
+        return Reserved
+
+
+    def buildPacket_offer(self, offer_ip, xid, mac):
+        ip_as_bytes = bytes(map(int, str(offer_ip).split('.')))
+        serverip = bytes(map(int, str("127.0.0.1").split('.')))
+
+        packet = b''
+        packet += b'\x02'  # opcode
+        packet += b'\x01'  # Hardware type: Ethernet
+        packet += b'\x06'  # Hardware address length: 6
+        packet += b'\x00'  # Hops: 0
+        # print("xidddd{}".format(xid))
+        xid_hex = hex(xid).split('x')[-1]
+        print(xid_hex)
+        packet += bytearray.fromhex(xid_hex)  # Transaction ID
+
+        packet += b'\x00\x00'  # Seconds elapsed: 0
+        packet += b'\x00\x00'  # Bootp flags
+        packet += b'\x00\x00\x00\x00'  # Client IP address: 0.0.0.0
+        packet += ip_as_bytes  # Your (client) IP address: 0.0.0.0
+        packet += serverip  # Next server IP address: 0.0.0.0
+        packet += b'\x00\x00\x00\x00'  # Relay agent IP address: 0.0.0.0
+        # packet += b'\x00\x26\x9e\x04\x1e\x9b'   #Client MAC address: 00:26:9e:04:1e:9b
+        # mac = self.connected_clients_list[xid]
+        mac = str(mac).replace(':', '')
+        packet += bytearray.fromhex(mac)
+        packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        packet += b'\x00' * 67  # Server host name
+        packet += b'\x00' * 125  # Boot file name
+        packet += b'\x63\x82\x53\x63'  # Magic cookie: DHCP
+        # DHCP IP Address
+        packet += b'\x35\x01\x02'
+
+        return packet
+
+
+    def buildPacket_Ack(self, offer_ip, xid, mac):
+        ip_as_bytes = bytes(map(int, str(offer_ip).split('.')))
+        serverip = bytes(map(int, str("127.0.0.1").split('.')))
+
+        packet = b''
+        packet += b'\x02'
+        packet += b'\x01'  # Hardware type: Ethernet
+        packet += b'\x06'  # Hardware address length: 6
+        packet += b'\x00'  # Hops: 0
+
+        xid_hex = hex(xid).split('x')[-1]
+        print(xid_hex)
+        packet += bytes.fromhex(xid_hex)  # Transaction ID
+
+        packet += b'\x00\x00'  # Seconds elapsed: 0
+        packet += b'\x00\x00'  # Bootp flags
+        packet += b'\x00\x00\x00\x00'  # Client IP address: 0.0.0.0
+        packet += ip_as_bytes  # Your (client) IP address: 0.0.0.0
+        packet += serverip  # Next server IP address: 0.0.0.0
+        packet += b'\x00\x00\x00\x00'  # Relay agent IP address: 0.0.0.0
+        # packet += b'\x00\x26\x9e\x04\x1e\x9b'   #Client MAC address: 00:26:9e:04:1e:9b
+        # mac = self.connected_clients_list[xid]
+        mac = str(mac).replace(':', '')
+        packet += bytes.fromhex(mac)
+        packet += b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        packet += b'\x00' * 67  # Server host name
+        packet += b'\x00' * 125  # Boot file name
+        packet += b'\x63\x82\x53\x63'  # Magic cookie: DHCP
+        # DHCP IP Address
+        packet += b'\x35\x01\x05'
+
+        return packet
+
+
+    def parse_packet_server(self, pkt):
+        xid = int(pkt[4:8].hex(), 16)
+        # print(pkt[28:44])
+        mac_byte = pkt[28:34]
+        mac_original = mac_byte.hex(":")
+
+        return xid, mac_original
+
+
+    def block_or_not(self, mac):
+        block = False
+        f = open("C:\\Users\\Asus\\PycharmProjects\\CN_P3\\configs.json")
+        data = json.load(f)
+        f.close()
+        if mac in data["black_list"]:
+            block = True
+        return block
+
+
+    def reserved_or_not(self, mac):
+        reserved = False
+        print(self.reserved)
+        print(mac)
+        if str(mac) in self.reserved:
+            reserved = True
+        return reserved
+
+
+    def show_clients(self):
+        pass
+        # while True:
+        #         show=input()
+        #         if show=="show_clients":
+        #             print(self.Serviced_ClientsInfo_print)
+
+
+    def lease(self, mac, ip, xid, index):
+        timeOut = self.lease_time
+        print("lease start for {}".format(mac))
+
+        while timeOut:
+            if mac not in self.client_ips:
+                self.client_ips[mac] = ip
+                self.OccupyIP.append(ip)
+                self.connected_clients_list[mac] = xid
+            mins, secs = divmod(timeOut, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+
+            time.sleep(1)
+            timeOut -= 1
+            self.Serviced_ClientsInfo_print[index][3] = timeOut
+            # print(self.Serviced_ClientsInfo_print)
+        print("lease expire for {}".format(mac))
+        self.OccupyIP.remove(ip)
+        self.waitIP.remove(ip)
+        self.connected_clients_list.pop(str(mac))
+        self.client_ips.pop(str(mac))
+
+
+def show(server):
+    print("hii")
+    while True:
+
+        show=input()
+        if show=="show_clients":
+            print(Server(server).Serviced_ClientsInfo_print)
+
+
+if __name__ == '__main__':
+    # Make sure all log messages show up
+
+    b = Server()
+
+
+    # t = threading.Thread(target=b.get_discovery())
+    #
+    # t.start()
+    b.get_discovery()
+
+    # t2 = threading.Thread(target=show(b))
+    #
+    # t2.start()
